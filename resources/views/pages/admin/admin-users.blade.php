13c6274af72da8bcd06ea2764b31a712e05eca49
elizzzz <ewize123@gmail.com>
2026-06-15 21:56:36 +0300

    Update admin-users.blade.php

--- resources/views/pages/admin/admin-users.blade.php
+++ resources/views/pages/admin/admin-users.blade.php
@@ -8,9 +8,10 @@ state(['users' => fn () => \App\Models\User::withTrashed()->orderBy('date_joined
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>{{ __('Admin - Registered Users') }}</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}?v={{ time() }}">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}?v={{ time() }}"></head>
+    <link rel="stylesheet" href="{{ secure_asset('css/style.css') }}?v={{ time() }}"></head>
 <body>
 
     <div class="admin-layout">
