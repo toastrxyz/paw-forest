b8a0089243fb3678a4978e13146131dcc0332a3d
elizzzz <ewize123@gmail.com>
2026-06-15 21:47:36 +0300

    Trust proxies for middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
             'employee' => \App\Http\Middleware\EmployeeMiddleware::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     
     ->withExceptions(function (Exceptions $exceptions): void {
